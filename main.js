841ae099adab802b3101a9b06df8c43cde342e8a
MaeFire <masked_phoenix@yahoo.com>
2025-08-07 13:41:02 -0500

    Create Book class/constructor. Add new book funct.
    Added the skeleton for library. Made a new class
    for book, including its constructor of what a book
    is. Function added that adds a book to library.
    Included a test function to be rewritten when
    wired to html.

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
 const myLibrary = [];
 
-function Book(title, author, pageAmount, isRead) {
-  if (!new.target) {
-    throw Error("You must use the 'new' operator to call the constructor");
+class Book {
+  constructor(title, author, pageAmount, isRead) {
+    if (!new.target) {
+      throw Error("You must use the 'new' operator to call the constructor");
+    }
+      this.title = title;
+      this.author = author;
+      this.pageAmount = pageAmount;
+      this.isRead = isRead;
+      this.id = crypto.randomUUID();
   }
-    this.title = title;
-    this.author = author;
-    this.pageAmount = pageAmount;
-    this.isRead = isRead;
-    this.id = crypto.randomUUID();
 }
 
 function addBookToLibrary(title, author, pageAmount, isRead) {
-  const newBook = new Book(title, author, pageAmount, isRead)
-  myLibrary.push(newBook);
-}
-
-Book.prototype.togglePageRead= function () {
-  this.isRead = !this.isRead;
+  const book = new Book(title, author, pageAmount, isRead);
+  myLibrary.push(book);
 }
 
 addBookToLibrary("Test Title","Arbitrary Author",42069,false);
+addBookToLibrary("Test Two","Arbitrary Writer",69,true);
+addBookToLibrary("Three Title","Arbiter Author",420,false);
 
-console.log(myLibrary[0]);
+myLibrary.forEach(book => console.log(book))//testing, refactor to display on page
+  // {
+  /*const bookId =
+    const bookTitle =
+    const bookAuthor =
+    const bookPageAmount =
+    const bookIsRead = 
+  */
+//}); 
+
+
+/* console.log(myLibrary[0]);
+console.log(myLibrary[1]);
+console.log(myLibrary[2]); */

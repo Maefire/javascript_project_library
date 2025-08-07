89618142b863fc0e15f816147f725deb4acbe0e8
MaeFire <masked_phoenix@yahoo.com>
2025-08-07 15:33:05 -0500

    Add forEach loop to display book info on the page

--- main.js
+++ main.js
@@ -22,7 +22,32 @@ addBookToLibrary("Test Title","Arbitrary Author",42069,false);
 addBookToLibrary("Test Two","Arbitrary Writer",69,true);
 addBookToLibrary("Three Title","Arbiter Author",420,false);
 
-myLibrary.forEach(book => console.log(book))//testing, refactor to display on page
+if (typeof document !== "undefined") {
+  const bookList = document.querySelector("#book-list");
+  bookList.textContent = '';
+  
+  myLibrary.forEach(book => {
+    const bookRow = document.createElement("tr");
+    bookRow.classList.add("book-row");
+    bookList.appendChild(bookRow);
+    // Title
+    const bookTitle = document.createElement("td");
+    bookTitle.textContent = book.title;
+    bookRow.appendChild(bookTitle); 
+    // Author
+    const bookAuthor = document.createElement("td");
+    bookAuthor.textContent = book.author;
+    bookRow.appendChild(bookAuthor);
+    // Page Amount
+    const bookPageAmount = document.createElement("td");
+    bookPageAmount.textContent = book.pageAmount;
+    bookRow.appendChild(bookPageAmount);
+    // Read Status (isRead)
+    const bookIsRead = document.createElement("td");
+    bookIsRead.textContent = book.isRead ? "Read" : "Not Read";//TODO classlist for checkbox
+    bookRow.appendChild(bookIsRead);
+  });
+}
   // {
   /*const bookId =
     const bookTitle =
